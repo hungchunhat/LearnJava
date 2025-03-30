557f6fba18dd383a48e4bb4fde9f077dad2a15cb
hungchunhat <maxtroll2001@gmail.com>
2025-03-31 01:22:48 +0700

    first commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from a Hưng Thái");
+        System.out.println("Hello World from Java");
+        //hưng đẹp trai vcl
+    }
+}
