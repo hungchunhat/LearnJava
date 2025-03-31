5f2c48195f8d25db56b4b0f1737b4c40a73b1a1f
hungchunhat <maxtroll2001@gmail.com>
2025-04-01 01:40:52 +0700

    Thêm Lab1 Practice

---
+++ src/Lab1/Lab1Practice.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+import java.util.Scanner;
+
+//nhap ten sinh vien ĐTB từ bàn phím
+/*In ra màn hinh với định dạng
+ * <tên sinh viên> có điểm = <điểm>*/
+public class Lab1Practice {
+    public static void main(String[] args) {
+        System.out.print("Enter the Student's name: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.print("Enter Student's average mark: ");
+        float mark = sc.nextFloat();
+        System.out.println(name + " có điểm = " + mark);
+    }
+}
