73a3b32236404ab4a050a1d857e9dda198acb3d6
hungchunhat <maxtroll2001@gmail.com>
2025-04-01 01:15:46 +0700

    sc.close()

--- src/Lab1/TwoSum.java
+++ src/Lab1/TwoSum.java
@@ -9,6 +9,8 @@ public class TwoSum {
         int a = sc.nextInt();
         System.out.print("Enter the second number: ");
         int b = sc.nextInt();
-        System.out.printf("The divide by %.1f is %d", Math.pow(2, b),(a >> b));
+        System.out.printf("The divide by %.0f is %d", Math.pow(2, b),(a >> b));
+        sc.close();
+        //sau khi dùng một thư viện xong thì nên đóng lại để tiết kiệm khoong gian bộ nhớ
     }
 }
