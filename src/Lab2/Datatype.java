4ba413e5735e680d6f90c28f068049bb802596ff
hungchunhat <maxtroll2001@gmail.com>
2025-04-02 01:13:12 +0700

    data type

---
+++ src/Lab2/Datatype.java
@@ -0,0 +1,14 @@
+package Lab2;
+
+public class Datatype {
+    public static void main(String[] args) {
+        int a;
+        byte a1;
+        short a2;
+        float b;
+        double c;
+        boolean d = true;//trong java không boolean không được để 1 và 0
+        char f;
+        String e;
+    }
+}
