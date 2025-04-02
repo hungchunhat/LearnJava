bd35709c30542609e63a218d5a03fe5bd4e71d35
hungchunhat <maxtroll2001@gmail.com>
2025-04-02 10:35:55 +0700

    change DataType

--- src/Lab2/Datatype.java
+++ src/Lab2/Datatype.java
@@ -5,8 +5,9 @@ public class Datatype {
         int a;
         byte a1;
         short a2;
-        float b;
-        double c;
+        long a4 = 1499999999999999999L;// nếu không thêm L ở cuối thì nó sẽ coi đây là int
+        float b = 3.666f;// nếu không khai báo f ở cuối thì nó tự động coi là double và báo lỗi vì float < double
+        double c ;
         boolean d = true;//trong java không boolean không được để 1 và 0
         char f;
         String e;
