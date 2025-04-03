1d1bdba69e2a62ffd22515a5991ef509d4919b06
hungchunhat <maxtroll2001@gmail.com>
2025-04-04 00:22:59 +0700

    Add Ex1

---
+++ src/Lab3/Ex1.java
@@ -0,0 +1,24 @@
+package Lab3;
+
+import java.util.Scanner;
+
+/*Nhập vào một số nguyên tố từ bàn phím kiểm tra xem có phải số nguyên tố*/
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("Chương trình kiểm tra số nguyên tố");
+        System.out.print("Nhập vào một số bất kì: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n < 0){
+            System.out.println("Số của bạn bé hơn 0? Bạn có bị ngu không?");
+            return;
+        }
+        for (int i = 2; i < n; i++) {
+            if(n%i == 0){
+                System.out.println("Đây không phải số nguyên tố");
+                return;//System.exit(0) mạnh hơn return
+            }
+        }
+        System.out.printf("%d là số nguyên tố",n);
+    }
+}
