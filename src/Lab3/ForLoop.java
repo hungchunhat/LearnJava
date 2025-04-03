f3badd7bb1db1163e69d6137c9bb7bb7dab2c46b
hungchunhat <maxtroll2001@gmail.com>
2025-04-03 23:56:55 +0700

    add ForLoop

---
+++ src/Lab3/ForLoop.java
@@ -0,0 +1,15 @@
+package Lab3;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        int[] clubs = {5,10,1,16,3};
+        Arrays.sort(clubs);
+        System.out.println(Arrays.toString(clubs));
+    }
+}
