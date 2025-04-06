d5d294723bacc74573ac9d6b6f6f60c5589454b5
hungchunhat <maxtroll2001@gmail.com>
2025-04-06 11:06:16 +0700

    LongestCommonPrefix

---
+++ src/LeetCode/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder prefix = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        for (int i = 0; i < (Math.min(first.length(), last.length())); i++) {
+            if (first.charAt(i) != last.charAt(i)) {
+                return prefix.toString();
+            }
+            prefix.append(first.charAt(i));
+        }
+        return prefix.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        String strTest = "Hello, World";
+        System.out.println(strTest.substring(7));//[)
+        String[] strs1 = {"dog", "racecar", "car"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
