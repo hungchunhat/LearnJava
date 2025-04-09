30c432b1b1e6df0759664bec4a1009256e00bd87
hungchunhat <maxtroll2001@gmail.com>
2025-04-09 22:12:01 +0700

    Better Set

--- src/leetcode/MinOperations.java
+++ src/leetcode/MinOperations.java
@@ -6,22 +6,14 @@ import java.util.Set;
 public class MinOperations {
     public int minOperations(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
-        boolean check = false;
         for (int num : nums) {
             if (num < k) {
                 return -1;
-            } else if (num == k) {
-                check = true;
-                set.add(num);
-            } else {
+            } else if (num > k) {
                 set.add(num);
             }
         }
-        if(check){
-            return set.size()-1;
-        }else{
-            return set.size();
-        }
+        return set.size();
     }
     public static void main(String[] args) {
         int[] nums = {5,2,5,4,5};
