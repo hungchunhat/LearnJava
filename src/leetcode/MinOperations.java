6b304b5fca89870e24fd3b9707f8caeee25d39a9
hungchunhat <maxtroll2001@gmail.com>
2025-04-10 01:30:50 +0700

    add Contractor

--- src/leetcode/MinOperations.java
+++ src/leetcode/MinOperations.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MinOperations {
+    public MinOperations(){}
     public int minOperations(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
