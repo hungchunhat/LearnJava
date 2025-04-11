c7414932a864498c964ff9cf4c5cb8fa2573e210
hungchunhat <maxtroll2001@gmail.com>
2025-04-11 16:20:23 +0700

    Update FirstApplication

--- src/testjava/FirstApplication.java
+++ src/testjava/FirstApplication.java
@@ -1,12 +1,12 @@
 package testjava;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class FirstApplication {
     public static void main(String[] args) {
         String result;
         result = JOptionPane.showInputDialog("Put your name here: ");
-        JOptionPane.showMessageDialog(null, "Hello "+result+" !");
+        JOptionPane.showMessageDialog(null, "Hello "+result+" !!!");
         System.exit(0);
     }
 }
