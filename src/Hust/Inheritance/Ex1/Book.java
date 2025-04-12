bc1105f0e6347168045004dc61ce3f87d4732c52
hungchunhat <maxtroll2001@gmail.com>
2025-04-12 15:45:34 +0700

    update Book --1

--- src/Hust/Inheritance/Ex1/Book.java
+++ src/Hust/Inheritance/Ex1/Book.java
@@ -5,31 +5,41 @@ public class Book {
     private String author;
     private int numberOfPages;
     private static int numberOfBook = 0;
-    public Book() {}
+
+    public Book() {
+    }
+
     public Book(String name, String author, int numberOfPages) {
         this.name = name;
         this.author = author;
         this.numberOfPages = numberOfPages;
         numberOfBook++;
     }
+
     public static int getNumberOfBook() {
         return numberOfBook;
     }
+
     public String getName() {
         return name;
     }
+
     public String getAuthor() {
         return author;
     }
+
     public int getNumberOfPages() {
         return numberOfPages;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public void setNumberOfPages(int numberOfPages) {
         this.numberOfPages = numberOfPages;
     }
