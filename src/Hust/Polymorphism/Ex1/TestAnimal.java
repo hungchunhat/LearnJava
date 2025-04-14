0e4f3f3b2e62731be2fb0b7bdeffa45d56080b13
hungchunhat <maxtroll2001@gmail.com>
2025-04-14 22:33:15 +0700

    comment on polymorphism

--- src/Hust/Polymorphism/Ex1/TestAnimal.java
+++ src/Hust/Polymorphism/Ex1/TestAnimal.java
@@ -17,8 +17,8 @@ public class TestAnimal {
         animal3.greeting();
 //        Animal animal4 = new Animal(); Error: Cant create an object for abstract class
         //Downcasting
-        Dog dog2 = (Dog) animal2;
-        BigDog bigDog2 = (BigDog) animal3;
+        Dog dog2 = (Dog) animal2;//downCasting
+        BigDog bigDog2 = (BigDog) animal3;//downCasting
         Dog dog3 = (Dog) animal3;
 //        Cat cat2 = (Cat) animal2; //Error: Cant animal2 is not instanceOf Cat
         dog2.greeting();
@@ -26,6 +26,5 @@ public class TestAnimal {
         dog2.greeting(bigDog2);
         bigDog2.greeting(dog2);
         bigDog2.greeting(bigDog1);
-        Animal animal4 = new Dog();
     }
 }
