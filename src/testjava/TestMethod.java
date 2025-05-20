a627b4b9024564e4f7c89202b3fc208295c62291
hungchunhat <maxtroll2001@gmail.com>
2025-05-20 21:56:26 +0700

    Try to change the Sequence of Parameter but fail

---
+++ src/testjava/TestMethod.java
@@ -0,0 +1,11 @@
+package testjava;
+
+public class TestMethod {
+    public static void add(int a, int b, String c) {
+        System.out.println( a + b + "và "+ c);
+    }
+    public static void main(String[] args) {
+//        add(a = 10,b = 100, c = 1000); Không thay đổi thứ các tham số truyền vào phương thức
+//        như php, nếu thay đổi thì nó giống như Overloading
+    }
+}
